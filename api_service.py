52e61fa6d24f39cbaee81b366411f7fa2f6b2ac5
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2025-05-19 16:20:50 +0300

    Update api_service.py

--- api_service.py
+++ api_service.py
@@ -5,6 +5,7 @@ class MCQGeneratorAPI:
         self.headers = {
             'accept': 'application/json',
             'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+            'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy5zdHVkb2N1LmNvbS9yZXN0LWFwaS92MS9hdXRoL2FkYXB0b3IiLCJpYXQiOjE3NDc2NjA0ODQsImV4cCI6MTc0NzY2NDA4NCwibmJmIjoxNzQ3NjYwNDg0LCJqdGkiOiJLaXFXSk1TaGVoM3YzcWd5Iiwic3ViIjoiNTYyMDgzNjIiLCJwcnYiOiI5ZjhhMjM4OWEyMGNhMDc1MmFhOWU5NTA5MzUxNTUxN2U5MGUxOTRjIn0.OsLFtMlTHGKjE6PsisKbb5E2H20IUaoMU-xScFCwQV9DDfCKK_p7bG7MwtNOy-rrRWXWtpeDDfaSywdOlEgZciOa7hyOGHdAEwbBna35KQ2K1a86UMITK926k4bRVlKEjfJHA7EWYRD-GyOoeqzl_EDIeJ9RU4U7njAmIgJt_rqfvE3AiFch_h8JSY600TF2jtyHlDrWaCOmnr0sjcEiuZNT8IhDEYleMJUl_naf9fRqkoJj6tkdOMkD7dxjTuhwfOgUdNdFUiiZmwSat-gOmhyhensg_HNZjg9Z81RMZhp4st8AijTdMKW9LIR7EsXZ4nHMrH3fE58il3p3QCti-A',
             'content-type': 'application/json',
             'origin': 'https://www.studocu.com',
             'priority': 'u=1, i',
@@ -16,9 +17,9 @@ class MCQGeneratorAPI:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
-            'x-forwarded-for': '105.37.78.202, 172.71.115.74, 172.71.115.74',
-            'x-request-id': '8294f6f7-603b-4598-9097-aa40d920de2d',
-            'x-session-id': '479ecfe8-ec70-47cc-aa87-8019fca4e56d',
+            'x-forwarded-for': '105.37.78.202, 162.158.116.51, 162.158.116.51',
+            'x-request-id': '5950f2f8-d422-4e07-80fc-918b13472bd9',
+            'x-session-id': 'fdc3b86a-5bf5-460a-9d2a-1987384704c3',
         }
         self.api_url = 'https://api.studocu.com/rest-api/v1/quizzes/v0/draft-quiz-questions'
 
